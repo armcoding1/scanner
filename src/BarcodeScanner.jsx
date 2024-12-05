5f50691a1f7618ed322dc524537159f96d8e04b6
Armen Baghdasaryan <armenbaghdasaryancoding@gmail.com>
2024-12-05 12:11:54 -0800

    i

--- src/BarcodeScanner.jsx
+++ src/BarcodeScanner.jsx
@@ -1,4 +1,4 @@
-import { BrowserQRCodeReader } from "@zxing/browser";
+import { BrowserQRCodeReader, BarcodeFormat } from "@zxing/browser";
 import { useState, useRef, useEffect } from "react";
 
 const BarcodeScanner = () => {
@@ -21,23 +21,41 @@ const BarcodeScanner = () => {
 
                 const selectedDeviceId = videoDevices[0].deviceId;
 
-                // Start the barcode scanner with video stream
-                codeReader.decodeFromVideoDevice(selectedDeviceId, videoRef.current, (scanResult, err) => {
-                    if (scanResult) {
-                        setResult(scanResult.text);
-                        console.log(scanResult.text);
+                // Start the scanner with 1D barcode formats in addition to QR codes
+                codeReader.decodeFromVideoDevice(
+                    selectedDeviceId, 
+                    videoRef.current, 
+                    {
+                        // You can specify the barcode formats you want to scan
+                        // Barcode formats such as UPC_A, EAN_13, CODE_128, etc.
+                        formats: [
+                            BarcodeFormat.UPC_A, 
+                            BarcodeFormat.UPC_E, 
+                            BarcodeFormat.EAN_13, 
+                            BarcodeFormat.EAN_8, 
+                            BarcodeFormat.CODE_128, 
+                            BarcodeFormat.CODE_39, 
+                            BarcodeFormat.ITF, 
+                            BarcodeFormat.CODABAR
+                        ]
+                    },
+                    (scanResult, err) => {
+                        if (scanResult) {
+                            setResult(scanResult.text);
+                            console.log(scanResult.text);
 
-                        if (scanResult.text.startsWith("http")) {
-                            window.location.href = scanResult.text;
+                            if (scanResult.text.startsWith("http")) {
+                                window.location.href = scanResult.text;
+                            }
+                        }
+                        if (err) {
+                            if (err.name !== "NotFoundException") {
+                                setError(err.message);
+                                console.error(err);
+                            }
                         }
                     }
-                    if (err) {
-                        if (err.name !== "NotFoundException") {
-                            setError(err.message);
-                            console.error(err);
-                        }
-                    }
-                });
+                );
             } catch (err) {
                 setError(err.message);
                 console.error(err);
